f5677e3c3a860da87d67963e0418b5d6ca40e6fd
rohanspatil <rohansp@hotmail.com>
2023-02-26 14:09:50 -0800

    Test Auto Code Complete

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.autos.PathPlannerAutos;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -59,7 +60,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = PathPlannerAutos.testAuto();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
